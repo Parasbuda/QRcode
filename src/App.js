fc9187e3875167a2395a198a2335189d6193f5d6
Parasbuda <parasbuda1234@gmail.com>
2021-01-12 14:41:30 +0545

    simple qr code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+import QRCode from "react-qr-code";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <QRCode
+        value="this is the test"
+        size={200}
+        level={"H"}
+        includeMargin={true}
+      />
     </div>
   );
-}
+};
 
 export default App;
